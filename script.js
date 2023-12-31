17b1eff6538b43a40a554a7df37e66712c2f588c
Christina Bernhardt <sozia76@web.de>
2023-12-31 22:12:30 +0100

    closePic

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let images = ['./img/1.jpg','./img/2.jpg','./img/3.jpg','./img/4.jpg','./img/5.j
 './img/19.jpg','./img/20.jpg','./img/21.jpg','./img/22.jpg','./img/23.jpg','./img/24.jpg','./img/25.jpg','./img/26.jpg','./img/27.jpg',
 './img/28.jpg','./img/29.jpg', './img/30.jpg','./img/31.jpg','./img/32.jpg'];
 
-function load() {
+function renderImages() {
   let imgGallerie = document.querySelector('#imgGallerie');
   imgGallerie.innerHTML = '';
   for (let i = 0; i < images.length; i++) {
     const image = images[i];
     imgGallerie.innerHTML += `
-   <img src="${image}" id="img" onclick="openPic(${i})">`;
+       <img src="${image}" id="img" onclick="openPic(${i})">`;
   }
 }
 
@@ -28,8 +28,42 @@ function openPic(i) {
   }
   function generateDetailViewHTML(i, image){
     return /* html */ `
+    <div class="detailViewWrapper">
+
     <div><button onclick="showPreviousPicture(${i})">links</button></div>
-    <div><img src=${image} alt="Bild"></div>
+    <div class="image-wrapper"><img src=${image} alt="Bild"></div>
     <div><button onclick="showNextPicture(${i})">rechts</button></div>
-    `
+    <div><button onclick="closePic()">Schließen</button></div>
+    </div>`
   }
+
+  function showPreviousPicture(i){
+    let image = images[i];
+    let detailContainer =  document.getElementById('onePic');
+    detailContainer.innerHTML = generateDetailViewHTML(i, image);
+    detailContainer.classList.remove('d-none');
+
+  
+  }
+  function showNextPicture(i){
+    let image = images[i];
+    let detailContainer =  document.getElementById('onePic');
+    detailContainer.innerHTML = generateDetailViewHTML(i, image);
+    detailContainer.classList.remove('d-none');
+
+  
+  }
+
+  function closePic(i) {
+    let image = images[i];
+    let detailContainer =  document.getElementById('onePic');
+    detailContainer.innerHTML = generateDetailViewHTML(i, image);
+    detailContainer.classList.add('d-none');
+  
+  
+    // document.getElementById('onePic').src = this.src;
+  // }
+  
+  //   function closePic(){
+  //       document.getElementById('onePic').classList.remove('showOnePic');
+    }
